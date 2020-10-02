02b44678e1187c9465e8384c8ad1f44f828203a0
Muricans <muricansisphat@gmail.com>
2020-10-01 22:20:57 -0500

    Added suggestion feature
    Any message sent to the bot will now be stored in the database as a feature, and will then have a separate message sent to a provided suggestion channel. In that suggestion channel, the message sent can be reacted to, which will send the reactor a message telling them to respond to the suggestor. Once a message has been received, the bot will then send that information to the suggestor through DMs.

--- events/messageEvent.js
+++ events/messageEvent.js
@@ -1,11 +1,11 @@
-const Discord =         require("discord.js")
-const fs =              require('fs');
+const Discord = require("discord.js")
+const fs = require('fs');
 
 //          -=- config files -=-      //
 
 //          -=- data files -=-        //
-const channels =        require("../data/channels.json");
-const roles =           require("../data/roles.json");
+const channels = require("../data/channels.json");
+const roles = require("../data/roles.json");
 
 //          -=- helper classes -=-        //
 
@@ -13,50 +13,70 @@ const badWords = [
     "idiot", "retard", "reetard", "rarded", "rarted"
 ]
 
+const sql = require('../functions/MySQL');
+const connection = sql.connect();
+
 module.exports = {
-    languageCheck: (bot ,message, user) => {
+    languageCheck: (bot, message, user) => {
         // if (message.member.roles.cache.has(r => r.id === roles.moderator)) {
         //     return;
         // }
-        
+
         message.content.split(' ').forEach(word => {
             if (badWords.includes(word)) {
                 message.delete();
                 let warning;
                 message.channel.send("Please watch your language!").then(msg => warning = msg)
-                setTimeout(function () { warning.delete()}, 10000);
-                
+                setTimeout(function () {
+                    warning.delete()
+                }, 10000);
+
                 const logChannel = bot.channels.cache.find(channel => channel.id === channels.logChannel)
                 const logEmbed = new Discord.MessageEmbed()
-                .setTitle("User warned for inappropriate language")
-                .setDescription(`${user} has been warned for cursing in ${message.channel}.`)
-                .addField("Message: ", `${message.content}`)
-                .addField("Blacklisted word:", `${word}`)
-                .setTimestamp(Date.now());
+                    .setTitle("User warned for inappropriate language")
+                    .setDescription(`${user} has been warned for cursing in ${message.channel}.`)
+                    .addField("Message: ", `${message.content}`)
+                    .addField("Blacklisted word:", `${word}`)
+                    .setTimestamp(Date.now());
                 logChannel.send(logEmbed)
             }
         });
     },
-    privateMessage: (bot, message, user) => {
-        const suggestionEmbed = new Discord.MessageEmbed()
-            .setTitle(`Suggestion submitted by ${user.tag}`)
-            .setDescription(message)
-            .setFooter(`User id: ${user.id}`)
-            .setTimestamp(Date.now())
-
-        bot.channels.cache.find(c => c.id === channels.suggestionsChannel).send(suggestionEmbed);
-
-        user.send("Hey, thanks for sending your feedback to us! We will take a look at it as soon as possible!")
+    privateMessage: async (bot, message, user) => {
+        const suggestion = message.content;
+        const sugChannel = await bot.channels.fetch(channels.suggestionsChannel);
+        const sug = await sql.select(connection, "suggestions");
+        if (sugChannel instanceof Discord.TextChannel) {
+            const suggestEmbed = new Discord.MessageEmbed()
+                .setTitle(`Suggestion #${Object.keys(sug).length + 1}`)
+                .addField('Submitted by', message.author)
+                .addField("Suggestion", suggestion)
+                .setFooter("React with 📧 to reply to the suggestor")
+                .setTimestamp(Date.now());
+            sugChannel.send(suggestEmbed).then(async sent => {
+                await sent.react('📧');
+                sql.insert(connection, "suggestions", "suggestion_user_id, suggestion_message, suggestion_message_id",
+                    `${message.author.id}, "${suggestion}", ${sent.id}`);
+            });
+        }
+        user.send('Hey, thanks for sending your feedback to us! We will take a look at it as soon as possible!');
+    },
+    suggestionReply: async (bot, message, responding, r) => {
+        bot.users.fetch(responding.suggestion.suggestion_user_id, true).then(user => {
+            user.send(`You received a response from a moderator: ${message.content}`);
+            r.splice(r.indexOf(responding), 1);
+        });
+        message.channel.send('Your response has been sent to the suggestor!');
     },
     messageEdit: (bot, oldMessage, newMessage, user) => {
         const editEmbed = new Discord.MessageEmbed()
-        .setTitle("Message has been edited")
-        .setDescription(`A message has been edited by ${user}`)
-        .addField(`Old message:`, oldMessage.content)
-        .addField(`New message:`, newMessage.content)
-        .setTimestamp(Date.now())
+            .setTitle("Message has been edited")
+            .setDescription(`A message has been edited by ${user}`)
+            .addField(`Old message:`, oldMessage.content)
+            .addField(`New message:`, newMessage.content)
+            .setTimestamp(Date.now())
 
         bot.channels.cache.find(c => c.id === channels.logChannel).send(editEmbed);
     }
-    
+
 }
--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-const Discord =     require("discord.js");
+const Discord = require("discord.js");
 
 //          -=- config files -=-        //
-const botconfig =   require("./botconfig.json");
+const botconfig = require("./botconfig.json");
 
 //          -=- Packages -=-            //
-const fs =          require('fs');
-const date =        require('date-and-time');
+const fs = require('fs');
+const date = require('date-and-time');
 
 //          -=- Events -=-            //
 const messageEvent = require("./events/messageEvent");
 
 //          -=- Data files -=-            //
-const roles=  require("./data/roles.json");
+const roles = require("./data/roles.json");
+const channels = require('./data/channels.json');
 
-const bot = new Discord.Client({disableEveryone: false}); //create a new client that is the bot
+const bot = new Discord.Client({
+    disableEveryone: false
+}); //create a new client that is the bot
+
+
+const sql = require('./functions/MySQL');
+const connection = sql.connect();
 
 /* Log that the bot is online and set it's status */
 bot.on("ready", async () => {
     let now = date.format(new Date(), 'YYYY/MM/DD HH:mm:ss');
     console.log(`[${now}] ${bot.user.username} is online`)
-    bot.user.setActivity("TeamToast", {type: "WATCHING"});
+    bot.user.setActivity("TeamToast", {
+        type: "WATCHING"
+    });
+    const suggestions = await sql.select(connection, "suggestions");
+    suggestions.map(sug => {
+        const channel = bot.channels.cache.get(channels.suggestionsChannel);
+        if (channel instanceof Discord.TextChannel)
+            channel.messages.fetch(sug.suggestion_message_id, true);
+    });
 });
 
 bot.commands = new Discord.Collection(); // Collection for all commands
@@ -29,6 +44,8 @@ bot.aliases = new Discord.Collection(); // Collection for all aliases of every c
 /* Create a list of all the subfolders that our commands will be in*/
 const modules = ['moderating', 'info', 'utility'];
 
+const responding = [];
+
 /* Command getter */
 modules.forEach(c => {
     fs.readdir(`./commands/${c}`, (err, files) => { //get all files in the "commands" folder
@@ -36,7 +53,7 @@ modules.forEach(c => {
         if (err) console.log(err) //if there is an error, log it in the console
 
         let jsfile = files.filter(f => f.split(".").pop() === "js") //filer all the files to only get the javascript files
-        if(jsfile.length <= 0){ //if there are no files found log in the console that no files (containing commands) have been found
+        if (jsfile.length <= 0) { //if there are no files found log in the console that no files (containing commands) have been found
             return console.log("[LOG] Couldn't find commands!");
         }
 
@@ -55,38 +72,65 @@ modules.forEach(c => {
 
 /* CommandFile excecutor */
 bot.on("message", async message => //if the bot sees a message (this can be in any text channel the bot has access to) do...
-{
-    
-    if (message.author.bot) { return; }
-
-    if(message.channel.type === "dm") 
     {
-        messageEvent.privateMessage(bot, message, message.author)
-        // Suggestions
-        // bot dms
-    }
 
-    let prefix = botconfig.prefix; //create a variable called prefix and assign the bots prefix to it
+        if (message.author.bot) {
+            return;
+        }
 
-    let messageArray = message.content.split(" ") //split the message that has been send into parts
-    let cmd = messageArray[0].toLowerCase(); //get the first part and name it "cmd"
-    let args = messageArray.slice(1); //get the message array minus the first index to remove the command from it
+        if (message.channel.type === "dm") {
+            if (responding.find(r => r.responding === message.author.id)) {
+                messageEvent.suggestionReply(bot, message, responding.find(r => r.responding === message.author.id), responding);
+            } else {
+                messageEvent.privateMessage(bot, message, message.author)
+                // Suggestions
+                // bot dms
+            }
+        }
 
-    if(message.content.startsWith(prefix) && message.author != bot)
-    {
-    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length))) //check if there is a command with the name that comes after the prefix, if so the code below will run the code in the file that corresponds to that command
-    if (commandfile) commandfile.run(bot, message, args)
-    else if (!commandfile) message.channel.send(`I am not able to find that command. Use "${prefix}help" to get a list of all commands.`)
-    } else {
-        messageEvent.languageCheck(bot, message, message.author)
-    }
+        let prefix = botconfig.prefix; //create a variable called prefix and assign the bots prefix to it
 
-});
+        let messageArray = message.content.split(" ") //split the message that has been send into parts
+        let cmd = messageArray[0].toLowerCase(); //get the first part and name it "cmd"
+        let args = messageArray.slice(1); //get the message array minus the first index to remove the command from it
+
+        if (message.content.startsWith(prefix) && message.author != bot) {
+            let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length))) //check if there is a command with the name that comes after the prefix, if so the code below will run the code in the file that corresponds to that command
+            if (commandfile) commandfile.run(bot, message, args)
+            else if (!commandfile) message.channel.send(`I am not able to find that command. Use "${prefix}help" to get a list of all commands.`)
+        } else {
+            messageEvent.languageCheck(bot, message, message.author)
+        }
+
+    });
 
 bot.on('messageUpdate', (oldMessage, newMessage) => {
-    if (newMessage.author.bot) { return; }
+    if (newMessage.author.bot) {
+        return;
+    }
     if (oldMessage.content != newMessage.content) {
         messageEvent.messageEdit(bot, oldMessage, newMessage, newMessage.author);
+    }
+});
+
+bot.on('messageReactionAdd', async (reaction, user) => {
+    if (user.bot) return;
+    const {
+        message,
+        emoji,
+        users
+    } = reaction;
+    if (emoji.name === '📧') {
+        const suggestions = await sql.select(connection, 'suggestions');
+        const suggestionId = suggestions.find(sug => sug.suggestion_message_id === message.id);
+        if (suggestionId) {
+            users.remove(user);
+            user.send('Respond to the suggestion: ');
+            responding.push({
+                responding: user.id,
+                suggestion: suggestionId
+            });
+        }
     }
 });
 
